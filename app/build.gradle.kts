7be4439c9ffc58740eee95bf7130fe299999fc52
Aryan <aryanrajivyms@gmail.com>
2026-04-22 14:08:55 +0530

    Bump version to 1.0.45(45)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.aryan.reader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 44
-        versionName = "1.0.43"
+        versionCode = 45
+        versionName = "1.0.45"
 
         resourceConfigurations += setOf("en", "ar", "de", "tr")
 
